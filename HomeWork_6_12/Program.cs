2e47c57dfcc57b8dab52283abe53f68be45775e0
Алексей Борисов <Aborisov96@yandex.ru>
2022-09-05 15:18:45 +0200

    init

--- HomeWork_6_12/Program.cs
+++ HomeWork_6_12/Program.cs
@@ -1,97 +1,196 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace HomeWork_6_12
 {
-    internal class Program
+    internal static class UserUtils
     {
-        static void Main(string[] args)
+        public static int GetNumber()
         {
+            bool isWork = true;
+
+            while (isWork)
+            {
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int number))
+                {
+                    isWork = false;
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте ещё раз!");
+                    isWork = true;
+                }
+            }
+            return 0;
         }
     }
 
-    class Zoopark
+    internal class Program
     {
-
+        private static void Main(string[] args)
+        {
+            Zoopark zoopark = new Zoopark();
+        }
     }
 
-    class Aviary
+    internal class Zoopark
     {
+        private List<Aviary> _aviaries;
 
+        public Zoopark()
+        {
+            _aviaries = new List<Aviary>();
+            _aviaries.Add(new Aviary(new Lions()));
+            _aviaries.Add(new Aviary(new Cows()));
+            _aviaries.Add(new Aviary(new Dogs()));
+            _aviaries.Add(new Aviary(new Monkees()));
+        }
+
+        public void Start()
+        {
+            bool isOpen = true;
+            Console.WriteLine("Добро подаловать в зоопарк!");
+
+            while (isOpen)
+            {
+                Console.Write("Выбирите вальер к которому хотите подойти:");
+                ShowAllAviaries();
+                int userInput = UserUtils.GetNumber();
+
+
+
+            }
+        }
+
+        private void ShowAllAviaries()
+        {
+            int countAviaries = 0;
+            foreach (Aviary aviary in _aviaries)
+            {
+                countAviaries++;
+                Console.WriteLine($"{countAviaries}.{aviary.Name}");
+            }
+        }
+
+        private void 
     }
 
-    abstract class Animal
+    internal class Aviary
     {
+        private List<Animal> _animals;
+        private int _countAnimals;
+        private Random _random;
+        private string _sound;
 
-        static string WomanSex = "Женский";
-        static string MenSex = "Мужской";
+        public string Name { get; private set; }
 
-        protected string Name;
-        protected string Sex;
-        protected string AnimalHowl;
+        public Aviary(Animal animal)
+        {
+            _random = new Random();
+            FillingAviary(animal);
+            _countAnimals = _animals.Count;
+            Name = $"Загон для {animal.Name}";
+            _sound = animal.AnimalHowl;
+        }
+
+        private void FillingAviary(Animal animal)
+        {
+            int minAnimals = 1;
+            int maxAnimals = 20;
+            _animals = new List<Animal>();
+            int countMount = _random.Next(minAnimals, maxAnimals);
+            int numberAnimalSpecies = 5;
+            int speciesAnimal = _random.Next(1, numberAnimalSpecies);
+
+            for (int i = 0; i < countMount; i++)
+            {
+                if (animal is Lions)
+                {
+                    _animals.Add(new Lions());
+                }
+                else if (animal is Cows)
+                {
+                    _animals.Add(new Cows());
+                }
+                else if (animal is Monkees)
+                {
+                    _animals.Add(new Monkees());
+                }
+                else if (animal is Dogs)
+                {
+                    _animals.Add(new Dogs());
+                }
+            }
+        }
+    }
+
+    internal abstract class Animal
+    {
+        private static string MenGender = "Мужской";
+        private static string WomanGender = "Женский";
+
+        protected string Gender { get; private set; }
+        public string AnimalHowl { get; protected set; }
+        public string Name { get; protected set; }
 
         public static string RandomSex()
         {
             Random random = new Random();
             int minValue = 1;
-            int maxValue = 2;
-            int sex = random.Next(minValue, maxValue);
+            int maxValue = 3;
+            int gender = random.Next(minValue, maxValue);
 
-            if (sex == 1)
+            if (gender == 1)
             {
-                return WomanSex;
+                return WomanGender;
             }
             else
             {
-                return MenSex;
+                return MenGender;
             }
         }
     }
 
-    class Lions : Animal
-    {
-        public Lions()
-        {
-            Name = "Лев";
-            Sex = RandomSex();
-            AnimalHowl = "РЫЫЫЫЫЫЫЫЫЫ";
-        }
-    }
-
-    class Cows : Animal
+    internal class Cows : Animal
     {
         public Cows()
         {
             Name = "Корова";
-            Sex = RandomSex();
+            Gender = RandomSex();
             AnimalHowl = "Мууу-муууу";
         }
     }
 
-    class Monkees : Animal
-    {
-        public Monkees()
-        {
-            Name = "Обезьяна";
-            Sex = RandomSex();
-            AnimalHowl = "УАА - УААА";
-        }
-    }
-
-    class Dogs:Animal
+    internal class Dogs : Animal
     {
         public Dogs()
         {
             Name = "Собака";
-            Sex = RandomSex();
+            Gender = RandomSex();
             AnimalHowl = "Гав - гав";
         }
+    }
 
+    internal class Lions : Animal
+    {
+        public Lions()
+        {
+            Name = "Лев";
+            Gender = RandomSex();
+            AnimalHowl = "РЫЫЫЫЫЫЫЫЫЫ";
+        }
+    }
+
+    internal class Monkees : Animal
+    {
+        public Monkees()
+        {
+            Name = "Обезьяна";
+            Gender = RandomSex();
+            AnimalHowl = "УАА - УААА";
+        }
     }
 }
-
-
