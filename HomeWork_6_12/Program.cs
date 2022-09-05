2d7a4d8b9b62cbd0b715ebf6f5c2b204e0c968d2
Алексей Борисов <Aborisov96@yandex.ru>
2022-09-05 12:52:40 +0200

    Добавьте файлы проекта.

---
+++ HomeWork_6_12/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HomeWork_6_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Zoopark
+    {
+
+    }
+
+    class Aviary
+    {
+
+    }
+
+    abstract class Animal
+    {
+
+        static string WomanSex = "Женский";
+        static string MenSex = "Мужской";
+
+        protected string Name;
+        protected string Sex;
+        protected string AnimalHowl;
+
+        public static string RandomSex()
+        {
+            Random random = new Random();
+            int minValue = 1;
+            int maxValue = 2;
+            int sex = random.Next(minValue, maxValue);
+
+            if (sex == 1)
+            {
+                return WomanSex;
+            }
+            else
+            {
+                return MenSex;
+            }
+        }
+    }
+
+    class Lions : Animal
+    {
+        public Lions()
+        {
+            Name = "Лев";
+            Sex = RandomSex();
+            AnimalHowl = "РЫЫЫЫЫЫЫЫЫЫ";
+        }
+    }
+
+    class Cows : Animal
+    {
+        public Cows()
+        {
+            Name = "Корова";
+            Sex = RandomSex();
+            AnimalHowl = "Мууу-муууу";
+        }
+    }
+
+    class Monkees : Animal
+    {
+        public Monkees()
+        {
+            Name = "Обезьяна";
+            Sex = RandomSex();
+            AnimalHowl = "УАА - УААА";
+        }
+    }
+
+    class Dogs:Animal
+    {
+        public Dogs()
+        {
+            Name = "Собака";
+            Sex = RandomSex();
+            AnimalHowl = "Гав - гав";
+        }
+
+    }
+}
+
+
